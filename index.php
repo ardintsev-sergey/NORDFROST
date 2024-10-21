2c91dc19503b5d03bb4479abbd96f0548c732893
Ардинцев Сергей Александрович <Ardincev_SA@diorit-aksay.ru>
2024-10-21 14:46:15 +0300

    desktop

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NORDFROST Landing</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <header class="header container">
+  <a href="#">
+    <img src="/img/nf-logo.png" alt="">
+  </a>
+  </header>
+  
+</body>
+</html>
